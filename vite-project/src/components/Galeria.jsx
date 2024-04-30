529024c58ec70fa2c63cfe7e9d2e85dc90c65ee7
brunao <131163437+brunozettermann@users.noreply.github.com>
2024-04-30 12:29:24 -0300

    first commit

---
+++ vite-project/src/components/Galeria.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Galeria(){
+    return(
+        <div className="Galeria">
+            <img src="ma1.PNG" alt="" />
+            <img src="ma2.PNG" alt="" />
+            <img src="ma3.PNG" alt="" />
+            <img src="ma4.PNG" alt="" />
+            <img src="ma5.PNG" alt="" />
+            <img src="ma6.PNG" alt="" />
+        </div>
+    )
+}
+
+export default Galeria
---
+++ vite-project/src/components/Rodape.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Rodape(){
+    return(
+        <footer>
+            <p>Bruno Zettermann 3-C</p>
+        </footer>
+    )
+}
+
+export default Rodape
